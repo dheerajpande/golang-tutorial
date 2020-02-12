dbc045cf62ea94003318affc588649a8a92c8bb0
Dheeraj Pande <dheerajpande@Dheerajs-MacBook-Pro.local>
2020-02-12 12:45:56 +0530

    adding variables file to show use of variables initialization

---
+++ basic-start/variables.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b int
+	first_name := "Jack"
+	var last_name string
+	last_name = "Cena"
+	fmt.Println("Name is ", first_name+" "+last_name) // string concatenation
+	a = 1
+	b = 2
+	fmt.Println("a + b = ", a+b)
+}
